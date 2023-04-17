dffe6052507afe948d96c8d1a832ae058a108578
isaaclee334 <125384108+isaaclee334@users.noreply.github.com>
2023-04-17 11:51:29 -0300

    Add files via upload

--- PRO-C110-Project-Boilerplate.py
+++ PRO-C110-Project-Boilerplate.py
@@ -28,27 +28,38 @@ while True:
 		
 		
 				
-		# Redimensione o quadro
-		resized_frame = cv2.resize(frame , (224,224))
-		# Expanda a dimensão do array junto com o eixo 0
-		resized_frame = np.expand_dims(resized_frame , axis = 0)
-		# Normalize para facilitar o processamento
-		resized_frame = resized_frame / 255
-		# Obtenha previsões do modelo
-		predictions = my.predict(resized_frame)
-		rock = int(predictions[0][0]*100)
-		paper = int(predictions[0][1]*100)
-		scissor = int(predictions[0][2]*100)
-		
-		# Exibindo os quadros capturados
-		cv2.imshow('feed' , frame)
+	# Redimensione o quadro
+	resized_frame = cv2.resize(frame , (224,224))
+	# Expanda a dimensão do array junto com o eixo 0
+	resized_frame = np.expand_dims(resized_frame , axis = 0)
+	# Normalize para facilitar o processamento
+	resized_frame = resized_frame / 255
+	# Obtenha previsões do modelo
+	predictions = my.predict(resized_frame)
+	rock = int(predictions[0][0]*100)
+	paper = int(predictions[0][1]*100)
+	scissor = int(predictions[0][2]*100)
 
-		# Aguardando 1ms
-		code = cv2.waitKey(1)
-		
-		# Se a barra de espaço foi pressionada, interrompa o loop
-		if code == 32:
-			break
+	#imprimir no video
+	if(rock>0.8):
+			cv2.putText(frame, "Pedra",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+	
+	elif(paper >0.8):
+		cv2.putText(frame, "Papel",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+	
+	elif(scissor>0.08):
+			cv2.putText(frame, "Tesoura",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+	print(f"Pedra:{rock}%, Papel:{paper}%,tesoura:{scissor}%")
+
+	# Exibindo os quadros capturadoss
+	cv2.imshow('feed' , frame)
+
+	# Aguardando 1ms
+	code = cv2.waitKey(1)
+	
+	# Se a barra de espaço foi pressionada, interrompa o loop
+	if code == 32:
+		break
 
 # Libere a câmera do software da aplicação
 camera.release()
